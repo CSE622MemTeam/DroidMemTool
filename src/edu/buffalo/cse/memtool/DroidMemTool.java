6248b638ef26320b785fdc032d7358ef61716e35
Brandon Ross <bwross@buffalo.edu>
2014-09-05 16:35:21 -0400

    Initial commit of empty project.

---
+++ src/edu/buffalo/cse/memtool/DroidMemTool.java
@@ -0,0 +1,15 @@
+package edu.buffalo.cse.memtool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DroidMemTool extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
