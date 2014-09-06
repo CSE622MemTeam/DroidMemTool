488028bed5b435c19da087b2acfb5cdc6007a354
Brandon Ross <bwross@buffalo.edu>
2014-09-06 12:36:54 -0400

    Added buttons and memory info to UI.

--- src/edu/buffalo/cse/memtool/DroidMemTool.java
+++ src/edu/buffalo/cse/memtool/DroidMemTool.java
@@ -1,15 +1,52 @@
 package edu.buffalo.cse.memtool;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.*;
 
-public class DroidMemTool extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.*;
+import android.os.*;
+import android.util.*;
+import android.view.*;
+import android.widget.*;
+
+public class DroidMemTool extends Activity {
+  TextView memoryUsage;
+  List<byte[]> bytes = new LinkedList<byte[]>();
+  
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    memoryUsage = (TextView) findViewById(R.id.memoryUsage);
+
+    Button oneButton = (Button) findViewById(R.id.allocateOne);
+    oneButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        allocateMemory(1);
+        updateInformation();
+      }
+    });
+
+    Button tenButton = (Button) findViewById(R.id.allocateTen);
+    tenButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        allocateMemory(10);
+        updateInformation();
+      }
+    });
+
+    updateInformation();
+  }
+
+  private void allocateMemory(int mb) {
+    Log.i("DroidMemTool", "Allocating "+mb+"MB");
+    bytes.add(new byte[mb * 1024 * 1024]);
+  }
+
+  private void updateInformation() {
+    Runtime info = Runtime.getRuntime();
+    long max = info.maxMemory() >> 20;
+    long usage = (info.totalMemory() - info.freeMemory()) >> 20;
+
+    memoryUsage.setText("Memory usage: "+usage+"MB / "+max+"MB");
+  }
 }
